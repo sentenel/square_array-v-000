dd588045b8fd1cffea21af3be3eaf46115211e58
Seth Wax <sethlwax@gmail.com>
2018-03-20 08:17:37 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,7 @@
 require 'pry'
 
 def square_array(array)
-  # sq_arr = Array.new
-  # array.each {|number| sq_arr << number**2}
-  # sq_arr
-
-  array.inject([]){|arr, n| arr << n}
-  binding.pry
+  sq_arr = Array.new
+  array.each {|number| sq_arr << number**2}
+  sq_arr
 end
